811d689a94eb2c51346dcc15ff251587897a3147
Vadym Shakhraichuk <shakhraichuk@gmail.com>
2021-07-06 22:53:10 -0700

    Assignment 3

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,16 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content_type', 'application/json');
+      res.json(users);
+    }
+  })
 });
 
 router.post('/signup', (req, res, next) => {
@@ -43,24 +51,17 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res) => {
-   if (req.session) {
-     req.session.destroy();
-     res.clearCookie('session-id');
-     res.redirect('/');
-   }
-   else {
-     var err = new Error('You are not logged in!');
-     err.status = 403;
-     next(err);
-   }
- });
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
 
 module.exports = router;
